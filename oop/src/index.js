0bd01d71053d15b98942dae21e78d9e7300bc307
Katya Kinyapina <kinyapina.katya28@gmail.com>
2024-07-31 13:11:15 +0700

    задание выполнено

--- oop/src/index.js
+++ oop/src/index.js
@@ -1,14 +1,1 @@
 import './style.css';
-/* 
-import Character from './types/Character';
-import Bowman from './types/Bowman';
-import Daemon from './types/Daemon';
-import Magician from './types/Magician';
-import Swordsman from './types/Swordsman';
-import Undead from './types/Undead';
-import Zombie from './types/Zombie';
-
-const playerZoro = new Bowman('Zoro', 'Bowman');
-playerZoro.levelUp();
-playerZoro.levelUp();
-console.log(playerZoro); */
